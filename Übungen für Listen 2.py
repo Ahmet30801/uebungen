3a6838fbc2570273a36ccec4ab819c5c614380b0
student <ahmet.toplar@sbs-herzogenaurach.de>
2024-10-22 09:12:27 +0200

    ex 15.10

--- Übungen für Listen 2.py
+++ Übungen für Listen 2.py
@@ -1,12 +1,13 @@
-'''
-#Aufgabe1
+
+#Aufgabe1 Summiere alle Zahlen in einer Liste
+
 zahlen=[5,3,8,2,10]
 summe = 0
 for zahl in zahlen:
     summe += zahl
 print(summe)
 
-#Aufgabe2
+#Aufgabe2 Finde das grösste Element in einer Liste
 
 zahlen2= [15, 22, 3, 47, 9, 5]
 temp=0
@@ -15,7 +16,7 @@ for zahl in zahlen2:
         temp=zahl
 print(temp)
 
-#Aufgabe3
+#Aufgabe3 Zähle wie oft ein bestimmtes Element in einer Liste vorkommt
 
 zahlen3=[4,2,4,6,4,7,8,4,1]
 for i in range(9):
@@ -23,7 +24,7 @@ for i in range(9):
     if res >= 0:
       print (f"Die Zahl {i} kommt {res} mal vor")
 
-#Aufgabe4
+#Aufgabe4 Verdopple alle Elemente in einer Liste
 
 zahlen4=[2, 5, 8, 1, 6]
 for zahl in zahlen4:
@@ -31,14 +32,14 @@ for zahl in zahlen4:
     print(f"Die Zahl {zahl} ist verdoppelt {erg}")
 
 
-#Aufgabe5
+#Aufgabe5 Alle Elemente rückwärrts ausgeben
 
 fruechte=["Apfel", "Banane", "Organe", "Mango"]
 fruechte.reverse()
 print(fruechte)
 
 
-#Aufgabe6
+#Aufgabe6 alle geraden Zahlen einer liste ausgeben
 
 zahlen5=[12, 7, 9, 16, 18, 21, 30]
 for zahl in zahlen5:
@@ -46,7 +47,9 @@ for zahl in zahlen5:
         print(zahl,end=";")
 
 print('\n')
-#Aufgabe7
+
+#Aufgabe7 Multipliziere jedes Element in der Liste mit 3
+
 
 zahlen6=[1, 4, 7, 10]
 for zahl in zahlen6:
